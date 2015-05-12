045fe5f85ebd6b29adafe6508f780e2d3865c48e
Vanessa Farraye <vanessafarraye@Vanessas-MacBook-Air-2.local>
2015-05-11 18:40:10 -0700

    homework

--- base.js
+++ base.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
   // code in here!
-  console.log("here's your data:", mockData)
+	$.get("mock_data.js", render(mockData))
+  
+	console.log("here's your data:", mockData)
+	function render(data){
+	var children = data.data
+	children.forEach(function(item,i){
+		var gif = item.images.fixed_height.url;
+		$("#blah").append("<img src='" + gif + "'>"); // mind the single vs. double quotes!
+		})  
+	}
+
+	$('#searchForm').submit(function(event){
+		event.preventDefault();
+		var inputVal = $(this.search).val();
+		console.log(inputVal)
+		$.get("http://api.giphy.com/v1/gifs/search?q="+inputVal+"&api_key=dc6zaTOxFJmzC", function (data){
+			var children = data.data
+			$("#blah").empty();
+			children.forEach(function(item,i){
+			var gif = item.images.fixed_height.url;	
+			$("#blah").append("<img src='" + gif + "'>");
+
+
+		})
+		})
+
 })
+
+});
+
+
